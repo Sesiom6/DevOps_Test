e42f39effa8c452188a79940e75356e9ca483607
Moisés Souza <62964010+Sesiom6@users.noreply.github.com>
2023-04-19 18:09:11 -0300

    fix

--- test/server.js
+++ test/server.js
@@ -14,7 +14,7 @@ var Message = mongoose.model('Message',{
   message : String
 })
 
-var dbUrl = 'mongodb://amkurian:amkurian1@ds257981.mlab.com:57981/simple-chat'
+var dbUrl = ''
 
 app.get('/messages', (req, res) => {
   Message.find({},(err, messages)=> {
@@ -61,7 +61,7 @@ io.on('connection', () =>{
   console.log('a user is connected')
 })
 
-mongoose.connect(dbUrl ,{useMongoClient : false} ,(err) => {
+mongoose.connect(dbUrl ,{useMongoClient : true} ,(err) => {
   console.log('mongodb connected',err);
 })
 
