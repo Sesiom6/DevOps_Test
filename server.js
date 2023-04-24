b4262a7baa0bce215a0081807d56a54fbcba7620
Moisés Souza <62964010+Sesiom6@users.noreply.github.com>
2023-04-24 14:02:20 -0300

    feat: autoresponse

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -7,64 +7,77 @@ var mongoose = require('mongoose');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }));
 
-var Message = mongoose.model('Message',{
-  name : String,
-  message : String
-})
-
-//var dbUrl = 'mongodb+srv://sesiom:JaLuSNq97shpRsDf@cluster0.xmoxaja.mongodb.net/?retryWrites=true&w=majority'
-
-app.get('/messages', (req, res) => {
-  Message.find({},(err, messages)=> {
-    res.send(messages);
-  })
-})
-
-app.get('/messages/:user', (req, res) => {
-  var user = req.params.user
-  Message.find({name: user},(err, messages)=> {
-    res.send(messages);
-  })
-})
-
-app.post('/messages', async (req, res) => {
-  try{
-    var message = new Message(req.body);
-
-    var savedMessage = await message.save()
-      console.log('saved');
-
-    var censored = await Message.findOne({message:'badword'});
-      if(censored)
-        await Message.remove({_id: censored.id})
-      else
-        io.emit('message', req.body);
-      res.sendStatus(200);
-  }
-  catch (error){
-    res.sendStatus(500);
-    return console.log('error',error);
-  }
-  finally{
-    console.log('Message Posted')
-    return 0
-  }
-
-})
-
-io.on('connection', () =>{
-  console.log('a user is connected')
-})
-
-mongoose.connect("mongodb+srv://sesiom:JaLuSNq97shpRsDf@cluster0.xmoxaja.mongodb.net/?retryWrites=true&w=majority", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}, (err) => {
-  console.log('mongodb connected', err);
+var Message = mongoose.model('Message', {
+  name: String,
+  message: String,
 });
 
+const autoResponses = {
+  'Oi': 'Olá! Como posso ajudar?',
+  'Como vai?': 'Estou bem e você?',
+  'Qual o seu nome?': 'Me chame de robôzin. Qual o seu nome?',
+  'Quem te criou?' : 'Meu criador se chama Moisés, também conhecido como Sesiom!'
+};
+
+app.get('/messages', (req, res) => {
+  Message.find({}, (err, messages) => {
+    res.send(messages);
+  });
+});
+
+app.get('/messages/:user', (req, res) => {
+  var user = req.params.user;
+  Message.find({ name: user }, (err, messages) => {
+    res.send(messages);
+  });
+});
+
+app.post('/messages', async (req, res) => {
+  try {
+    var message = new Message(req.body);
+
+    var savedMessage = await message.save();
+    console.log('saved');
+
+    const autoResponse = autoResponses[req.body.message];
+    if (autoResponse) {
+      const responseMessage = new Message({
+        name: 'Robôzin',
+        message: autoResponse
+      });
+      await responseMessage.save();
+      io.emit('message', responseMessage);
+      console.log(`Autoresponse: ${autoResponse}`);
+    } else {
+      io.emit('message', req.body);
+    }
+
+    res.sendStatus(200);
+  } catch (error) {
+    res.sendStatus(500);
+    return console.error('Error', error);
+  } finally {
+    console.log('Message Posted');
+  }
+});
+
+io.on('connection', () => {
+  console.log('A user is connected');
+});
+
+mongoose.connect(
+  'mongodb+srv://sesiom:JaLuSNq97shpRsDf@cluster0.xmoxaja.mongodb.net/?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  err => {
+    console.log('MongoDB connected', err);
+  }
+);
+
 var server = http.listen(3000, () => {
-  console.log('server is running on port', server.address().port);
+  console.log('Server is running on port', server.address().port);
 });
