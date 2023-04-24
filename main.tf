62f69b1cc3f67080f66121f1e2259011dc7a1ed6
Moisés Souza <62964010+Sesiom6@users.noreply.github.com>
2023-04-24 18:00:14 -0300

    fix: ajustes no terraform

--- main.tf
+++ main.tf
@@ -2,22 +2,71 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_vpc" "simple_chat_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "simple-chat-vpc"
+  }
+}
+
+resource "aws_subnet" "simple_chat_subnet" {
+  vpc_id     = aws_vpc.simple_chat_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "simple-chat-subnet"
+  }
+}
+
 resource "aws_security_group" "simple_chat_sg" {
   name_prefix = "sc-sg"
+  vpc_id      = aws_vpc.simple_chat_vpc.id
 
   ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "all"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "all"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "all"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
+//resource "tls_private_key" "mykey" {
+//  algorithm = "RSA"
+//  rsa_bits  = 4096
+//}
+
+resource "aws_key_pair" "simple_chat_key" {
+  key_name   = "simple-chat_key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDwiSLF2e81ucwW0ZWaY8aBOBxjhTv4AudrEF44JvkPyy4+Bd8v6yM4Q7+lTE26vSBeT0zs0W2ktogTbU9moHI8btEKu+X0vy37RMLijjeLuzs11/C5xE7ltUr75Kfl/ZXm3NgcOWiSDXsILyU/oC7XXwCOgU/R6S4hqMa7tIkNDhMtGfgUKfe5kUgK+8NoMqMx+XUZnm/lmBTalQxhgP9XEXlUPEBeJUlGgvArr/C0R6+qYr1dzMGfsAyBEw6XwTMdFV4Cu4SsW5fWm1/DqEABpP2kNDRd3J4Nr6+vBbgVuRwHzikcXQn9oGxX96sFBbgoG52BDc/wI8PxXD6CDZ+/ pichau@Moises"
+
+  tags = {
+    Name = "simple-chat_key"
+  }
+}
 
 resource "aws_instance" "simple-chat" {
-  ami           = "ami-0c94855ba95c71c99"
-  instance_type = "t2.micro"
-  user_data = <<-EOF
+  ami                         = "ami-02396cdd13e9a1257"
+  instance_type               = "t2.micro"
+  vpc_security_group_ids      = [aws_security_group.simple_chat_sg.id]
+  subnet_id                   = aws_subnet.simple_chat_subnet.id
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.simple_chat_key.key_name
+  user_data                   = <<-EOF
               #!/bin/bash
               sudo apt-get update
               sudo apt-get install -y docker.io
@@ -31,5 +80,9 @@ EOF
 }
 
 output "public_ip" {
-  value = aws_instance.example.public_ip
+  value = aws_instance.simple-chat.public_ip
 }
+
+//output "public_key_openssh" {
+//  value = tls_private_key.mykey.public_key_openssh
+//}
