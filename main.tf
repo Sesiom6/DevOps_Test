23f4d8c8685020ce6b163dd4368535fdd51d9be4
Moisés Souza <62964010+Sesiom6@users.noreply.github.com>
2023-04-24 10:39:57 -0300

    fix: connection string

--- main.tf
+++ main.tf
@@ -2,16 +2,28 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_security_group" "simple_chat_sg" {
+  name_prefix = "sc-sg"
+
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "all"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
 resource "aws_instance" "simple-chat" {
   ami           = "ami-0c94855ba95c71c99"
   instance_type = "t2.micro"
-  user_data = <<EOF
+  user_data = <<-EOF
               #!/bin/bash
               sudo apt-get update
               sudo apt-get install -y docker.io
               sudo docker pull sesiom6/simple-chat-app
               sudo docker run -d -p 80:80 sesiom6/simple-chat-app
-              EOF
+EOF
 
   tags = {
     Name = "simple-chat-app-instance"
